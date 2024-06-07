c7f0660f4f76c1e35f5c16b302c0ff4315007975
Korneli <108990408+kornelikkk@users.noreply.github.com>
2024-06-07 15:09:05 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,302 @@
+﻿namespace Desmos2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl = new System.Windows.Forms.TabControl();
+            this.addPage = new System.Windows.Forms.Button();
+            this.removePage = new System.Windows.Forms.Button();
+            this.addTextBox = new System.Windows.Forms.TextBox();
+            this.addDotButton = new System.Windows.Forms.Button();
+            this.textBoxY = new System.Windows.Forms.TextBox();
+            this.textBoxX = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.newZoneButton = new System.Windows.Forms.Button();
+            this.rbSpline = new System.Windows.Forms.RadioButton();
+            this.rbLine = new System.Windows.Forms.RadioButton();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tabControl
+            // 
+            this.tabControl.Location = new System.Drawing.Point(0, 3);
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.Size = new System.Drawing.Size(724, 538);
+            this.tabControl.TabIndex = 1;
+            // 
+            // addPage
+            // 
+            this.addPage.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.addPage.Location = new System.Drawing.Point(739, 24);
+            this.addPage.Name = "addPage";
+            this.addPage.Size = new System.Drawing.Size(202, 49);
+            this.addPage.TabIndex = 2;
+            this.addPage.Text = "Add new page";
+            this.addPage.UseVisualStyleBackColor = true;
+            this.addPage.Click += new System.EventHandler(this.addPage_Click);
+            // 
+            // removePage
+            // 
+            this.removePage.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.removePage.Location = new System.Drawing.Point(739, 139);
+            this.removePage.Name = "removePage";
+            this.removePage.Size = new System.Drawing.Size(202, 49);
+            this.removePage.TabIndex = 3;
+            this.removePage.Text = "Remove this page";
+            this.removePage.UseVisualStyleBackColor = true;
+            this.removePage.Click += new System.EventHandler(this.removePage_Click);
+            // 
+            // addTextBox
+            // 
+            this.addTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.addTextBox.Location = new System.Drawing.Point(792, 96);
+            this.addTextBox.Name = "addTextBox";
+            this.addTextBox.Size = new System.Drawing.Size(100, 29);
+            this.addTextBox.TabIndex = 4;
+            // 
+            // addDotButton
+            // 
+            this.addDotButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.addDotButton.Location = new System.Drawing.Point(758, 294);
+            this.addDotButton.Name = "addDotButton";
+            this.addDotButton.Size = new System.Drawing.Size(164, 47);
+            this.addDotButton.TabIndex = 7;
+            this.addDotButton.Text = "Add new dot";
+            this.addDotButton.UseVisualStyleBackColor = true;
+            this.addDotButton.Click += new System.EventHandler(this.addDotButton_Click);
+            // 
+            // textBoxY
+            // 
+            this.textBoxY.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxY.Location = new System.Drawing.Point(845, 347);
+            this.textBoxY.Name = "textBoxY";
+            this.textBoxY.Size = new System.Drawing.Size(47, 29);
+            this.textBoxY.TabIndex = 6;
+            // 
+            // textBoxX
+            // 
+            this.textBoxX.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxX.Location = new System.Drawing.Point(790, 347);
+            this.textBoxX.Name = "textBoxX";
+            this.textBoxX.Size = new System.Drawing.Size(47, 29);
+            this.textBoxX.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(740, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(203, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Enter graphic name if you want";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(779, 438);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(58, 29);
+            this.textBox1.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(885, 438);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(58, 29);
+            this.textBox2.TabIndex = 10;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox4.Location = new System.Drawing.Point(885, 482);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(58, 29);
+            this.textBox4.TabIndex = 12;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox3.Location = new System.Drawing.Point(779, 482);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(58, 29);
+            this.textBox3.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(733, 447);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 17);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Min X";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(840, 447);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 17);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Min Y";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(730, 491);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 17);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Max X";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(837, 491);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 17);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Max Y";
+            // 
+            // newZoneButton
+            // 
+            this.newZoneButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.newZoneButton.Location = new System.Drawing.Point(758, 391);
+            this.newZoneButton.Name = "newZoneButton";
+            this.newZoneButton.Size = new System.Drawing.Size(173, 41);
+            this.newZoneButton.TabIndex = 17;
+            this.newZoneButton.Text = "Apply new zone";
+            this.newZoneButton.UseVisualStyleBackColor = true;
+            this.newZoneButton.Click += new System.EventHandler(this.newZoneButton_Click);
+            // 
+            // rbSpline
+            // 
+            this.rbSpline.AutoSize = true;
+            this.rbSpline.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.rbSpline.Location = new System.Drawing.Point(758, 216);
+            this.rbSpline.Name = "rbSpline";
+            this.rbSpline.Size = new System.Drawing.Size(81, 28);
+            this.rbSpline.TabIndex = 18;
+            this.rbSpline.TabStop = true;
+            this.rbSpline.Text = "Spline";
+            this.rbSpline.UseVisualStyleBackColor = true;
+            this.rbSpline.CheckedChanged += new System.EventHandler(this.rbSpline_CheckedChanged);
+            // 
+            // rbLine
+            // 
+            this.rbLine.AutoSize = true;
+            this.rbLine.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.rbLine.Location = new System.Drawing.Point(758, 250);
+            this.rbLine.Name = "rbLine";
+            this.rbLine.Size = new System.Drawing.Size(64, 28);
+            this.rbLine.TabIndex = 19;
+            this.rbLine.TabStop = true;
+            this.rbLine.Text = "Line";
+            this.rbLine.UseVisualStyleBackColor = true;
+            this.rbLine.CheckedChanged += new System.EventHandler(this.rbLine_CheckedChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(789, 196);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(89, 17);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "Graphic type";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(956, 539);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.rbLine);
+            this.Controls.Add(this.rbSpline);
+            this.Controls.Add(this.newZoneButton);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.addDotButton);
+            this.Controls.Add(this.textBoxY);
+            this.Controls.Add(this.textBoxX);
+            this.Controls.Add(this.addTextBox);
+            this.Controls.Add(this.removePage);
+            this.Controls.Add(this.addPage);
+            this.Controls.Add(this.tabControl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TabControl tabControl;
+        private System.Windows.Forms.Button addPage;
+        private System.Windows.Forms.Button removePage;
+        private System.Windows.Forms.TextBox addTextBox;
+        private System.Windows.Forms.Button addDotButton;
+        private System.Windows.Forms.TextBox textBoxY;
+        private System.Windows.Forms.TextBox textBoxX;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button newZoneButton;
+        private System.Windows.Forms.RadioButton rbSpline;
+        private System.Windows.Forms.RadioButton rbLine;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Data.SQLite;
+using Dapper;
+using System.Windows.Forms.DataVisualization.Charting;
+
+
+namespace Desmos2
+{
+    public partial class Form1 : Form
+    {
+        private SQLiteConnection connection;
+        List<(float, float)> points = new List<(float, float)>();
+        public Form1()
+        {
+            InitializeComponent();
+
+            addPage_Click(null, null);
+            rbSpline.Select();
+        }
+        private void Draw()
+        {
+            Chart ch = (Chart)tabControl.SelectedTab.Controls["ch"];
+            ch.Series.Clear();
+            Series s = new Series("f(x)");
+            s.ChartType = SeriesChartType.Spline;
+
+            s.ChartArea = "f(x)";
+            s.BorderWidth = 3;
+
+            foreach (var point in points)
+            {
+                s.Points.AddXY(point.Item1, point.Item2);
+            }
+
+            ch.Series.Add(s);
+        }
+        private void CreateTable(string name)
+        {
+            connection = new SQLiteConnection("Data Source=DB.db;");
+            connection.Open();
+
+            connection.Execute(@"CREATE TABLE IF NOT EXISTS " + name + @" ( 
+                                    id	INTEGER PRIMARY KEY,
+	                                x	REAL NOT NULL,
+	                                y	REAL NOT NULL)");
+
+            connection.Close();
+        }
+        private void PullFromTable(string name)
+        {
+            connection = new SQLiteConnection("Data Source=DB.db;");
+            connection.Open();
+            using (SQLiteCommand command = new SQLiteCommand($@"SELECT x , y  FROM {name}", connection))
+            {
+                using (SQLiteDataReader reader = command.ExecuteReader())
+                {
+                    points.Clear();
+                    while (reader.Read())
+                    {
+                        points.Add((reader.GetFloat(0), reader.GetFloat(1)));
+                    }
+                }
+            }
+        }
+        private void SaveTable(string name)
+        {
+            connection = new SQLiteConnection("Data Source=DB.db;");
+            connection.Open();
+
+            connection.Execute($@"INSERT INTO {name} ( x , y ) VALUES ( @x , @y )", new { x = points[points.Count - 1].Item1, y = points[points.Count - 1].Item2 });
+
+            connection.Close();
+        }
+        private void removePage_Click(object sender, EventArgs e)
+        {
+            connection = new SQLiteConnection("Data Source=DB.db;");
+            connection.Open();
+            string name = tabControl.SelectedTab.Text;
+            connection.Execute($@"DROP TABLE {name}");
+
+            connection.Close();
+            int index = tabControl.SelectedIndex;
+            tabControl.SelectedIndex = 0;
+            tabControl.TabPages.RemoveAt(index);
+        }
+
+        private void addPage_Click(object sender, EventArgs e)
+        {
+            TabPage tp;
+            if (addTextBox.Text != "")
+            {
+                tp = new TabPage(addTextBox.Text);
+            }
+            else
+            {
+                tp = new TabPage("P");
+            }
+            tabControl.TabPages.Add(tp);
+            Chart ch = new Chart();
+            ch.Name = "ch";
+            ch.Dock = DockStyle.Fill;
+            ch.ChartAreas.Clear();
+            ch.ChartAreas.Add(new ChartArea("f(x)"));
+            CreateTable(tp.Text);
+            PullFromTable(tp.Text);
+
+
+            ch.ChartAreas[0].AxisX.Crossing = 0;
+            ch.ChartAreas[0].AxisY.Crossing = 0;
+
+            ch.ChartAreas[0].AxisX.Minimum = -5;
+            ch.ChartAreas[0].AxisY.Minimum = -5;
+            ch.ChartAreas[0].AxisX.Maximum = 5;
+            ch.ChartAreas[0].AxisY.Maximum = 5;
+
+            ch.ChartAreas[0].AxisY.LineWidth = 2;
+            ch.ChartAreas[0].AxisX.LineWidth = 2;
+
+            Series s = new Series("f(x)");
+            if (rbSpline.Checked)
+                s.ChartType = SeriesChartType.Spline;
+            else
+                s.ChartType = SeriesChartType.Line;
+
+            ch.Legends.Add(s.Name);
+            s.ChartArea = "f(x)";
+            s.BorderWidth = 3;
+
+            foreach (var point in points)
+            {
+                s.Points.AddXY(point.Item1, point.Item2);
+            }
+            ch.Series.Clear();
+            ch.Series.Add(s);
+
+            tp.Controls.Add(ch);
+            tp.Controls.SetChildIndex(ch, 5);
+
+            textBox1.Text = "-5";
+            textBox2.Text = "-5";
+            textBox4.Text = "5";
+            textBox3.Text = "5";
+
+        }
+
+        private void addDotButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                float x = float.Parse(textBoxX.Text);
+                float y = float.Parse(textBoxY.Text);
+                points.Add((x, y));
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Неверный формат ввода");
+                return;
+            }
+            SaveTable(tabControl.SelectedTab.Text);
+            Draw();
+        }
+
+        private void newZoneButton_Click(object sender, EventArgs e)
+        {
+            Chart ch = (Chart)tabControl.SelectedTab.Controls["ch"];
+            try
+            {
+                ch.ChartAreas[0].AxisX.Minimum = double.Parse(textBox1.Text);
+                ch.ChartAreas[0].AxisY.Minimum = double.Parse(textBox2.Text);
+                ch.ChartAreas[0].AxisX.Maximum = double.Parse(textBox4.Text);
+                ch.ChartAreas[0].AxisY.Maximum = double.Parse(textBox3.Text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Неверный формат ввода");
+                return;
+            }
+            Draw();
+        }
+
+        private void rbSpline_CheckedChanged(object sender, EventArgs e)
+        {
+            Chart ch = (Chart)tabControl.SelectedTab.Controls["ch"];
+            ch.Series[0].ChartType = SeriesChartType.Spline;
+        }
+
+        private void rbLine_CheckedChanged(object sender, EventArgs e)
+        {
+            Chart ch = (Chart)tabControl.SelectedTab.Controls["ch"];
+            ch.Series[0].ChartType = SeriesChartType.Line;
+        }
+    }
+}
